5693b474babb704e69841c8f93518e14d498852f
Irene Li <li.irene1@gmail.com>
2020-05-26 15:19:37 -0700

    fix json loader issue

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,4 @@
-// import extraInfoData from '../constants/extraInfo.json';
-const extraInfoData = {
-  "whenToUse" : [
-    "Assessing a patient’s renal function",
-    "Prescribing a drug that is renally metabolized"
-  ],
-  "pearlsPitfalls": {
-    "title": "From Dan Brown, PharmD, at Palm Beach Atlantic University, the primary author of the functional range of creatinine clearance paper",
-    "text": "“The Cockcroft-Gault equation remains the gold standard after almost 40 years, despite inaccuracies that arise from variations in body composition among patients. Those who understand potential sources of error can adjust accordingly.”"
-  }
-}
+import extraInfoData from 'constants/extraInfo';
 
 const initialState = {
   extraInfo: extraInfoData,
@@ -51,6 +41,7 @@ function rootReducer(state = initialState, action) {
       result: action.result
     };
   }
+
   return state;
 };
 
